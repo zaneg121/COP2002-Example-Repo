6d2063337e0b4a8f8388f3ca9ad71a02fb7b418e
zaneg121 <zagillis1@gmail.com>
2024-09-02 19:11:25 -0400

    Adding a python file to repository
    Adding the original file.

---
+++ COP2002-Example-RepoZ.py
@@ -0,0 +1,7 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Creating a project for GitHub
+>>> # By Zane Gillis
+>>> 
+>>> print("COP2002 FALL 2030")
+COP2002 FALL 2030
